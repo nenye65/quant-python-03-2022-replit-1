fc2100ae54abc2558824d14c5ed865255012f3cb
Alex Coleman <alexjcoleman@gmail.com>
2022-03-11 15:44:34 +0000

    add setup.py script for downloading data

---
+++ setup.py
@@ -0,0 +1,18 @@
+import requests
+
+URL = "https://ndownloader.figshare.com/files/10717177"
+
+def main():
+    """
+    Quick script for downloading required data
+    """
+
+    req = requests.get(URL, stream=True)
+
+    with open("surveys.csv", 'wb') as fd:
+        for chunk in req.iter_content(chunk_size=128):
+            fd.write(chunk)
+
+
+if __name__ == '__main__':
+    main()
